cb9bff995076500535365fa6cb7c986971cdb5b6
jojo259 <95320642+jojo259@users.noreply.github.com>
2023-05-05 16:22:11 +0100

    tidy mapmanager

--- src/game/mapmanager.ts
+++ src/game/mapmanager.ts
@@ -27,63 +27,49 @@ export function getTileValue(x: number, y: number): number {
 	return tileMap?.[Math.floor(y)]?.[Math.floor(x)] ?? 1;
 }
 
-export function generateMap(x: number, y: number): number[][] {
+function generateMap(width: number, height: number): number[][] {
 	let mapArray: number[][] = [];
-	console.log(mapArray);
-	for(let i = 0; i < y; i++){
+	for(let i = 0; i < width; i++){
 		mapArray[i] = [];
-		//console.log(mapArray[i]);
-		for(let j = 0; j < x; j++){
-			//console.log(mapArray[i][j]);
+		for(let j = 0; j < height; j++){
 			mapArray[i][j] = 1;
 		}
 	}
-	console.log(mapArray.length);
-	console.log(mapArray[0].length);
-	//return mapArray;
-	return generateNoise(mapArray, x, y);
+	return generateNoise(mapArray, width, height);
 }
 
-export function generateNoise(mapArray: number[][], x: number, y: number): number[][] {
-	for(let i = 1; i < y-1; i++){
-		for(let j = 1; j < x-1; j++){
+function generateNoise(mapArray: number[][], width: number, height: number): number[][] {
+	for(let i = 1; i < height - 1; i++) {
+		for(let j = 1; j < width - 1; j++) {
 			let random = Math.floor(Math.random() * 100);
-			if(random > mapDensity){
+			if(random > mapDensity) {
 				mapArray[i][j] = 0;
 			}
-			if(random <= mapDensity){
+			if(random <= mapDensity) {
 				mapArray[i][j] = 1;
 			}
 		}
 	}
-	//return mapArray;
-	return generate(mapArray, x, y);
+	return runCellularAutomaton(mapArray, width, height);
 }
 
-export function generate(mapArray: number[][], width: number, height: number): number[][]{ 
-	//let tempArray: number[][] = [];
-	for(let i = 0; i < genAmount; i++){
+function runCellularAutomaton(mapArray: number[][], width: number, height: number): number[][] { 
+	for (let i = 0; i < genAmount; i++){
 		let tempArray = mapArray.map(inner => inner.slice());
-		/*
-		for(let t = 0; t < mapArray.length; t++){
-			tempArray[t] = mapArray[t].slice();
-		}*/
-		for(let j = 1; j < width-1; j++){
-			for(let k = 1; k < height-1; k++){
-				let neighbour_walls = 0;
-				for(let x = j-1; x < j+2; x++){
-					for(let y = k-1; y < k+2; y++){
-						if(y != j && x != k){
-							if(tempArray[y][x] == 1){
-								neighbour_walls++;
-							}
+		for (let j = 1; j < width - 1; j++){
+			for (let k = 1; k < height - 1; k++){
+				let neighborWalls = 0;
+				for (let x = j-1; x < j+2; x++){
+					for (let y = k-1; y < k+2; y++){
+						if (y != j && x != k){
+							neighborWalls += tempArray[x][y];
 						}
 					}
 				}
-				if(neighbour_walls > 4){
+				if (neighborWalls > 4) {
 					mapArray[k][j] = 1;
 				}
-				else{
+				else {
 					mapArray[k][j] = 0;
 				}
 			}
@@ -94,7 +80,7 @@ export function generate(mapArray: number[][], width: number, height: number): n
 
 export let tileMap = generateMap(mapWidth, mapHeight);
 
-export function generateSpawn(mapArray: number[][], width: number, height: number): number[]{
+function generateSpawn(mapArray: number[][], width: number, height: number): number[]{
 	while(true){
 		let wallFound = false
 		let randomWidth = Math.floor(Math.random() * width);
@@ -118,39 +104,3 @@ export function generateSpawn(mapArray: number[][], width: number, height: numbe
 		}
 	}
 }
-
-/*
-export let tileMap: number[][] = [
-	[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-	[1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-	[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-	[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1],
-	[1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-	[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-	[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-	[1,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-	[1,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-	[1,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-	[1,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,0,0,1],
-	[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,1],
-	[1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,1],
-	[1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,1],
-	[1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-	[1,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-	[1,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-	[1,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-	[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1],
-	[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1],
-	[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-	[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
-	[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,1,1],
-	[1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,1],
-	[1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,1],
-	[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,1,1,0,0,0,0,0,0,0,0,1],
-	[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,1,1,0,0,0,0,0,0,0,0,1],
-	[1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,1,1],
-	[1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,1,1,1],
-	[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,1,1,1],
-	[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1],
-	[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-];*/
