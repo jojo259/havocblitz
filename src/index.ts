2fd9b210c57cb4ab41f60ae54d0f41727fac9ba4
jojo259 <95320642+jojo259@users.noreply.github.com>
2023-05-10 19:13:19 +0100

    tick lag warning

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,14 @@ import { resetCanvasSize } from "./page/canvas"
 import { findRandomPeer } from "./net/peerfinder"
 import { sendData } from "./net/peermanager";
 import { addClientPlayerEntity } from "./game/entitymanager";
-import { considerTicking } from "./game/tickingmanager"
+import { considerTicking, considerTickingIntervalMs } from "./game/tickingmanager"
 import { addListeners } from "./game/inputtracker";
 
 resetCanvasSize();
 
 setInterval(findRandomPeer, 1000)
 
-setInterval(considerTicking, 1);
+setInterval(considerTicking, considerTickingIntervalMs);
 
 addClientPlayerEntity();
 addListeners();
