98009a8fff29a9d9ed0806f77890f2ca6aa7530a
jojo259 <95320642+jojo259@users.noreply.github.com>
2023-05-11 13:38:04 +0100

    tidy

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import { resetCanvasSize } from "./page/canvas"
-import { findRandomPeer } from "./net/peerfinder"
+import { resetCanvasSize } from "./page/canvas";
+import { findRandomPeer } from "./net/peerfinder";
 import { sendData } from "./net/peermanager";
 import { addClientPlayerEntity } from "./game/entitymanager";
-import { considerTicking, considerTickingIntervalMs } from "./game/tickingmanager"
+import { considerTicking, considerTickingIntervalMs } from "./game/tickingmanager";
 import { addListeners } from "./game/inputtracker";
 
 resetCanvasSize();
 
-setInterval(findRandomPeer, 1000)
+setInterval(findRandomPeer, 1000);
 
 setInterval(considerTicking, considerTickingIntervalMs);
 
