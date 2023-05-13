a0438547013713e8ef580bf74da5da6b321bc19e
jojo259 <95320642+jojo259@users.noreply.github.com>
2023-05-13 12:09:46 +0100

    do not attempt to connect to client peer

--- src/net/peerfinder.ts
+++ src/net/peerfinder.ts
@@ -1,5 +1,5 @@
 import { Peer } from "peerjs";
-import { peerIdPrefix, maxPeerIdNum, clientPeer, peerConnections, ingestPotentialPeerConnection } from "./peermanager";
+import { peerIdPrefix, maxPeerIdNum, clientPeer, peerConnections, ingestPotentialPeerConnection, clientPeerId } from "./peermanager";
 
 let atRandomPeerNum = 0;
 
@@ -15,6 +15,11 @@ export function findRandomPeer(){
 	let randomPeerId = peerIdPrefix + atRandomPeerNum;
 	console.log("attempting to connect to random peer with id " + randomPeerId);
 	console.log("current connections: " + Object.keys(peerConnections).join(", "));
+
+	if (randomPeerId == clientPeerId) {
+		console.log("random peer is client peer, skipping");
+		return;
+	}
 
 	if (peerConnections.hasOwnProperty(randomPeerId)) {
 		console.log("already connected to this peer");
