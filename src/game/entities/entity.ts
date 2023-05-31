3583ee4fb4363bd81d3b785709ec4d4e6b45cdad
jojo259 <95320642+jojo259@users.noreply.github.com>
2023-05-31 17:20:49 +0100

    improve entity find spawn

--- src/game/entities/entity.ts
+++ src/game/entities/entity.ts
@@ -27,6 +27,9 @@ export abstract class Entity {
 	}
 
 	findSpawn(): void {
+		if (!tileMap) {
+			console.error("map does not exist yet");
+		}
 		for (let i = 0; i < 8192; i++) {
 			let newPosX = 0;
 			let newPosY = 0;
@@ -34,15 +37,28 @@ export abstract class Entity {
 				newPosX = Math.floor(Math.random() * mapWidth);
 				newPosY = Math.floor(Math.random() * mapHeight);
 			}
-			let wallFound = false;
+			let positionInEmptyTiles = true;
 			for(let x = newPosX-1; x <= newPosX+1; x++){
 				for(let y = newPosY-1; y <= newPosY+1; y++){
 					if(tileMap[x][y] != 0){
-						wallFound = true;
+						positionInEmptyTiles = false;
+						break;
 					}
 				}
+				if (!positionInEmptyTiles) {
+					break;
+				}
 			}
-			if (wallFound) {
+			if (!positionInEmptyTiles) {
+				continue;
+			}
+			let positionHasTilesBelow = false;
+			for(let cy = newPosY+1; cy <= mapHeight; cy++){
+				if(tileMap[newPosX][cy] > 0){
+					positionHasTilesBelow = true;
+				}
+			}
+			if (!positionHasTilesBelow) {
 				continue;
 			}
 			this.posX = newPosX;
